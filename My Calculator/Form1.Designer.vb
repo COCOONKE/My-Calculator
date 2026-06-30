deac2f3c71ac4ed55b82b23badb97e48bd9aa5db
HomePC <HomePC@PC>
2026-06-30 23:33:14 +0300

    Add project files.

---
+++ My Calculator/Form1.Designer.vb
@@ -0,0 +1,436 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblDisplay = New Label()
+        btnMR = New Button()
+        btnMPlus = New Button()
+        btnMS = New Button()
+        btnMC = New Button()
+        btnMMinus = New Button()
+        Button6 = New Button()
+        btnReciprocal = New Button()
+        btnNegate = New Button()
+        btnPercent = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        btnCE = New Button()
+        btnC = New Button()
+        btnBackspace = New Button()
+        Button16 = New Button()
+        btnSquare = New Button()
+        Button18 = New Button()
+        Button19 = New Button()
+        Button20 = New Button()
+        btnSqrt = New Button()
+        Button22 = New Button()
+        Button23 = New Button()
+        Button24 = New Button()
+        Button25 = New Button()
+        Button26 = New Button()
+        Button27 = New Button()
+        Button28 = New Button()
+        Button29 = New Button()
+        btnEquals = New Button()
+        SuspendLayout()
+        ' 
+        ' lblDisplay
+        ' 
+        lblDisplay.FlatStyle = FlatStyle.Flat
+        lblDisplay.Font = New Font("Segoe UI", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblDisplay.Location = New Point(12, 9)
+        lblDisplay.Name = "lblDisplay"
+        lblDisplay.Size = New Size(580, 64)
+        lblDisplay.TabIndex = 0
+        lblDisplay.Text = "0"
+        lblDisplay.TextAlign = ContentAlignment.MiddleRight
+        ' 
+        ' btnMR
+        ' 
+        btnMR.FlatAppearance.BorderSize = 0
+        btnMR.FlatStyle = FlatStyle.Flat
+        btnMR.Location = New Point(130, 97)
+        btnMR.Name = "btnMR"
+        btnMR.Size = New Size(94, 29)
+        btnMR.TabIndex = 1
+        btnMR.Text = "MR"
+        btnMR.UseVisualStyleBackColor = True
+        ' 
+        ' btnMPlus
+        ' 
+        btnMPlus.FlatAppearance.BorderSize = 0
+        btnMPlus.FlatStyle = FlatStyle.Flat
+        btnMPlus.Location = New Point(239, 97)
+        btnMPlus.Name = "btnMPlus"
+        btnMPlus.Size = New Size(94, 29)
+        btnMPlus.TabIndex = 2
+        btnMPlus.Text = "M+"
+        btnMPlus.UseVisualStyleBackColor = True
+        ' 
+        ' btnMS
+        ' 
+        btnMS.FlatAppearance.BorderSize = 0
+        btnMS.FlatStyle = FlatStyle.Flat
+        btnMS.Location = New Point(466, 97)
+        btnMS.Name = "btnMS"
+        btnMS.Size = New Size(94, 29)
+        btnMS.TabIndex = 3
+        btnMS.Text = "MS"
+        btnMS.UseVisualStyleBackColor = True
+        ' 
+        ' btnMC
+        ' 
+        btnMC.FlatAppearance.BorderSize = 0
+        btnMC.FlatStyle = FlatStyle.Flat
+        btnMC.Location = New Point(12, 97)
+        btnMC.Name = "btnMC"
+        btnMC.Size = New Size(94, 29)
+        btnMC.TabIndex = 4
+        btnMC.Text = "MC"
+        btnMC.UseVisualStyleBackColor = True
+        ' 
+        ' btnMMinus
+        ' 
+        btnMMinus.FlatAppearance.BorderSize = 0
+        btnMMinus.FlatStyle = FlatStyle.Flat
+        btnMMinus.Location = New Point(350, 97)
+        btnMMinus.Name = "btnMMinus"
+        btnMMinus.Size = New Size(94, 29)
+        btnMMinus.TabIndex = 5
+        btnMMinus.Text = "M-"
+        btnMMinus.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.FlatAppearance.BorderSize = 0
+        Button6.FlatStyle = FlatStyle.Flat
+        Button6.Location = New Point(585, 97)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(94, 29)
+        Button6.TabIndex = 6
+        Button6.Text = "M^"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' btnReciprocal
+        ' 
+        btnReciprocal.Location = New Point(47, 199)
+        btnReciprocal.Name = "btnReciprocal"
+        btnReciprocal.Size = New Size(94, 29)
+        btnReciprocal.TabIndex = 7
+        btnReciprocal.Text = "1/x"
+        btnReciprocal.UseVisualStyleBackColor = True
+        ' 
+        ' btnNegate
+        ' 
+        btnNegate.Location = New Point(47, 396)
+        btnNegate.Name = "btnNegate"
+        btnNegate.Size = New Size(94, 29)
+        btnNegate.TabIndex = 8
+        btnNegate.Text = "+/-"
+        btnNegate.UseVisualStyleBackColor = True
+        ' 
+        ' btnPercent
+        ' 
+        btnPercent.Location = New Point(47, 147)
+        btnPercent.Name = "btnPercent"
+        btnPercent.Size = New Size(94, 29)
+        btnPercent.TabIndex = 9
+        btnPercent.Text = "%"
+        btnPercent.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Button10.Location = New Point(47, 350)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(94, 29)
+        Button10.TabIndex = 10
+        Button10.Text = "1"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Button11.Location = New Point(47, 304)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(94, 29)
+        Button11.TabIndex = 11
+        Button11.Text = "4"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button12.Location = New Point(47, 252)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(94, 29)
+        Button12.TabIndex = 12
+        Button12.Text = "7"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' btnCE
+        ' 
+        btnCE.Location = New Point(172, 147)
+        btnCE.Name = "btnCE"
+        btnCE.Size = New Size(94, 29)
+        btnCE.TabIndex = 13
+        btnCE.Text = "CE"
+        btnCE.UseVisualStyleBackColor = True
+        ' 
+        ' btnC
+        ' 
+        btnC.Location = New Point(309, 147)
+        btnC.Name = "btnC"
+        btnC.Size = New Size(94, 29)
+        btnC.TabIndex = 14
+        btnC.Text = "C"
+        btnC.UseVisualStyleBackColor = True
+        ' 
+        ' btnBackspace
+        ' 
+        btnBackspace.BackColor = SystemColors.ActiveCaption
+        btnBackspace.Location = New Point(426, 147)
+        btnBackspace.Name = "btnBackspace"
+        btnBackspace.Size = New Size(94, 29)
+        btnBackspace.TabIndex = 15
+        btnBackspace.Text = "Backspace"
+        btnBackspace.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Button16.Location = New Point(172, 396)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(94, 29)
+        Button16.TabIndex = 16
+        Button16.Text = "0"
+        Button16.UseVisualStyleBackColor = True
+        ' 
+        ' btnSquare
+        ' 
+        btnSquare.Location = New Point(172, 199)
+        btnSquare.Name = "btnSquare"
+        btnSquare.Size = New Size(94, 29)
+        btnSquare.TabIndex = 17
+        btnSquare.Text = "x²"
+        btnSquare.UseVisualStyleBackColor = True
+        ' 
+        ' Button18
+        ' 
+        Button18.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Button18.Location = New Point(172, 252)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(94, 29)
+        Button18.TabIndex = 18
+        Button18.Text = "8"
+        Button18.UseVisualStyleBackColor = True
+        ' 
+        ' Button19
+        ' 
+        Button19.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Button19.Location = New Point(172, 304)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(94, 29)
+        Button19.TabIndex = 19
+        Button19.Text = "5"
+        Button19.UseVisualStyleBackColor = True
+        ' 
+        ' Button20
+        ' 
+        Button20.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Button20.Location = New Point(172, 350)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(94, 29)
+        Button20.TabIndex = 20
+        Button20.Text = "2"
+        Button20.UseVisualStyleBackColor = True
+        ' 
+        ' btnSqrt
+        ' 
+        btnSqrt.Location = New Point(309, 199)
+        btnSqrt.Name = "btnSqrt"
+        btnSqrt.Size = New Size(94, 29)
+        btnSqrt.TabIndex = 21
+        btnSqrt.Text = "√"
+        btnSqrt.UseVisualStyleBackColor = True
+        ' 
+        ' Button22
+        ' 
+        Button22.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Button22.Location = New Point(309, 250)
+        Button22.Name = "Button22"
+        Button22.Size = New Size(94, 29)
+        Button22.TabIndex = 22
+        Button22.Text = "9"
+        Button22.UseVisualStyleBackColor = True
+        ' 
+        ' Button23
+        ' 
+        Button23.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Button23.Location = New Point(309, 300)
+        Button23.Name = "Button23"
+        Button23.Size = New Size(94, 29)
+        Button23.TabIndex = 23
+        Button23.Text = "6"
+        Button23.UseVisualStyleBackColor = True
+        ' 
+        ' Button24
+        ' 
+        Button24.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Button24.Location = New Point(309, 350)
+        Button24.Name = "Button24"
+        Button24.Size = New Size(94, 29)
+        Button24.TabIndex = 24
+        Button24.Text = "3"
+        Button24.UseVisualStyleBackColor = True
+        ' 
+        ' Button25
+        ' 
+        Button25.Location = New Point(309, 396)
+        Button25.Name = "Button25"
+        Button25.Size = New Size(94, 29)
+        Button25.TabIndex = 25
+        Button25.Text = "."
+        Button25.UseVisualStyleBackColor = True
+        ' 
+        ' Button26
+        ' 
+        Button26.Location = New Point(426, 199)
+        Button26.Name = "Button26"
+        Button26.Size = New Size(94, 29)
+        Button26.TabIndex = 26
+        Button26.Text = "÷"
+        Button26.UseVisualStyleBackColor = True
+        ' 
+        ' Button27
+        ' 
+        Button27.Location = New Point(426, 250)
+        Button27.Name = "Button27"
+        Button27.Size = New Size(94, 29)
+        Button27.TabIndex = 27
+        Button27.Text = "x"
+        Button27.UseVisualStyleBackColor = True
+        ' 
+        ' Button28
+        ' 
+        Button28.Location = New Point(426, 300)
+        Button28.Name = "Button28"
+        Button28.Size = New Size(94, 29)
+        Button28.TabIndex = 28
+        Button28.Text = "-"
+        Button28.UseVisualStyleBackColor = True
+        ' 
+        ' Button29
+        ' 
+        Button29.Location = New Point(426, 350)
+        Button29.Name = "Button29"
+        Button29.Size = New Size(94, 29)
+        Button29.TabIndex = 29
+        Button29.Text = "+"
+        Button29.UseVisualStyleBackColor = True
+        ' 
+        ' btnEquals
+        ' 
+        btnEquals.BackColor = SystemColors.ActiveCaption
+        btnEquals.Location = New Point(426, 396)
+        btnEquals.Name = "btnEquals"
+        btnEquals.Size = New Size(94, 29)
+        btnEquals.TabIndex = 30
+        btnEquals.Text = "="
+        btnEquals.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnEquals)
+        Controls.Add(Button29)
+        Controls.Add(Button28)
+        Controls.Add(Button27)
+        Controls.Add(Button26)
+        Controls.Add(Button25)
+        Controls.Add(Button24)
+        Controls.Add(Button23)
+        Controls.Add(Button22)
+        Controls.Add(btnSqrt)
+        Controls.Add(Button20)
+        Controls.Add(Button19)
+        Controls.Add(Button18)
+        Controls.Add(btnSquare)
+        Controls.Add(Button16)
+        Controls.Add(btnBackspace)
+        Controls.Add(btnC)
+        Controls.Add(btnCE)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(btnPercent)
+        Controls.Add(btnNegate)
+        Controls.Add(btnReciprocal)
+        Controls.Add(Button6)
+        Controls.Add(btnMMinus)
+        Controls.Add(btnMC)
+        Controls.Add(btnMS)
+        Controls.Add(btnMPlus)
+        Controls.Add(btnMR)
+        Controls.Add(lblDisplay)
+        Name = "Form1"
+        Text = "Calculator"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents lblDisplay As Label
+    Friend WithEvents btnMR As Button
+    Friend WithEvents btnMPlus As Button
+    Friend WithEvents btnMS As Button
+    Friend WithEvents btnMC As Button
+    Friend WithEvents btnMMinus As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents btnReciprocal As Button
+    Friend WithEvents btnNegate As Button
+    Friend WithEvents btnPercent As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents btnCE As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btnBackspace As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents btnSquare As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents btnSqrt As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button23 As Button
+    Friend WithEvents Button24 As Button
+    Friend WithEvents Button25 As Button
+    Friend WithEvents Button26 As Button
+    Friend WithEvents Button27 As Button
+    Friend WithEvents Button28 As Button
+    Friend WithEvents Button29 As Button
+    Friend WithEvents btnEquals As Button
+
+End Class
---
+++ My Calculator/Form1.vb
@@ -0,0 +1,105 @@
+﻿Public Class Form1
+    Dim operand1 As Double = 0
+    Dim currentOperator As String = ""
+    Dim isCalculationDone As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblDisplay.Text = "0"
+    End Sub
+
+    ' --- 1. NUMBER BUTTONS & DOT ---
+    ' Make sure your 0-9 and Dot buttons have this selected in their Click Event (Lightning Bolt)
+    Private Sub Number_Click(sender As Object, e As EventArgs) Handles Button16.Click, Button10.Click, Button20.Click, Button24.Click, Button23.Click, Button19.Click, Button11.Click, Button12.Click, Button18.Click, Button22.Click, Button25.Click
+        Dim btn = CType(sender, Button)
+
+        ' If a previous calculation was done, clear screen completely for a fresh start
+        If isCalculationDone Then
+            lblDisplay.Text = ""
+            isCalculationDone = False
+        End If
+
+        ' Prevent appending multiple leading zeros
+        If lblDisplay.Text = "0" AndAlso btn.Text <> "." Then
+            lblDisplay.Text = btn.Text
+        Else
+            ' Append the number or dot straight to the display string
+            If btn.Text = "." Then
+                ' Only allow a dot if the current segment doesn't have one
+                If Not lblDisplay.Text.EndsWith(".") Then
+                    lblDisplay.Text &= "."
+                End If
+                Exit Sub
+            End If
+            lblDisplay.Text &= btn.Text
+        End If
+    End Sub
+
+    ' --- 2. OPERATOR BUTTONS (+, -, x, ÷) ---
+    ' Make sure your +, -, x, ÷ buttons have this selected in their Click Event (Lightning Bolt)
+    Private Sub Operator_Click(sender As Object, e As EventArgs) Handles Button29.Click, Button28.Click, Button27.Click, Button26.Click
+        Dim btn = CType(sender, Button)
+
+        ' Don't allow starting with an operator if display is empty or error
+        If lblDisplay.Text = "0" OrElse lblDisplay.Text.Contains("Error") Then Exit Sub
+
+        ' If an operator is already present in the string, ignore extra clicks
+        If lblDisplay.Text.Contains("+") OrElse lblDisplay.Text.Contains("-") OrElse lblDisplay.Text.Contains("x") OrElse lblDisplay.Text.Contains("÷") Then
+            Exit Sub
+        End If
+
+        ' Save the first number and the operator symbol, then append it to the screen text
+        currentOperator = btn.Text.Trim
+        lblDisplay.Text &= " " & currentOperator & " "
+        isCalculationDone = False
+    End Sub
+
+    ' --- 3. EQUALS BUTTON (=) ---
+    ' Double click your '=' button and make sure it points here
+    Private Sub btnEquals_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
+        Try
+            ' Split the display text by spaces to read parts (e.g., "9", "x", "5")
+            Dim parts As String() = lblDisplay.Text.Split(New Char() {" "c}, StringSplitOptions.RemoveEmptyEntries)
+
+            ' Ensure we have all 3 parts needed for a basic equation
+            If parts.Length < 3 Then Exit Sub
+
+            operand1 = Convert.ToDouble(parts(0))
+            Dim op As String = parts(1)
+            Dim operand2 As Double = Convert.ToDouble(parts(2))
+            Dim result As Double = 0
+
+            Select Case op
+                Case "+"
+                    result = operand1 + operand2
+                Case "-"
+                    result = operand1 - operand2
+                Case "x", "×"
+                    result = operand1 * operand2
+                Case "÷", "/"
+                    If operand2 = 0 Then
+                        lblDisplay.Text = "Cannot divide by zero"
+                        Exit Sub
+                    Else
+                        result = operand1 / operand2
+                    End If
+                Case Else
+                    Exit Sub
+            End Select
+
+            ' This formatting shows "9 x 5 = 45" completely on screen for your assignment snapshot!
+            lblDisplay.Text &= " = " & result.ToString()
+            isCalculationDone = True
+
+        Catch ex As Exception
+            lblDisplay.Text = "Error"
+        End Try
+    End Sub
+
+    ' --- 4. CLEAR (C) BUTTON ---
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+        lblDisplay.Text = "0"
+        operand1 = 0
+        currentOperator = ""
+        isCalculationDone = False
+    End Sub
+End Class
